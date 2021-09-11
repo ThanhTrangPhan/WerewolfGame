2aa412aea118b6dd4d0f43416b0c414dcdc609d9
Phan200 <chualeo58@gmail.com>
2021-09-11 18:07:57 +0700

    change the createGroup method

--- src/Controller/GroupDAO.java
+++ src/Controller/GroupDAO.java
@@ -19,32 +19,69 @@ import java.util.ArrayList;
  *
  * @author demo
  */
-public class GroupDAO extends DAO{
-    public GroupDAO(){
+public class GroupDAO extends DAO {
+
+    public GroupDAO() {
         super();
     }
+
     // create a new group
-    public boolean createGroup(Group group){
-        String sql = "INSERT INTO tblGroup(tblGroup.name,tblGroup.founderName,tblGroup.timeStarted)"
-                + " VALUES (?, ?,?);";
-        try{
-            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+    public boolean createGroup(Group group) {
+        String sqlcreate = "INSERT INTO tblGroup(tblGroup.name,tblGroup.founderName,tblGroup.timeStarted)"
+                + " VALUES (?, ?,?)";
+        String sqlAddMember = "INSERT INTO tblGroupMember(tblGroupMember.timeJoined,tblGroupMember.GroupID,tblGroupMember.playerID) "
+                + "VALUES (?, ?,?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sqlcreate, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, group.getName());
             ps.setString(2, group.getFounderName());
             ps.setString(3, group.getTimeStarted());
-            
+
             ps.executeUpdate();
             ResultSet groupID = ps.getGeneratedKeys();
             if (groupID.next()) {
                 group.setId(groupID.getInt(1));
+                ps = conn.prepareStatement(sqlAddMember, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, String.valueOf(java.time.LocalDateTime.now()));
+                ps.setInt(2, group.getId());
+                ps.setInt(3, group.getMember().get(0).getPlayer().getId());
+                ps.executeUpdate();
+
+                // id of new member
+                ResultSet member_id = ps.getGeneratedKeys();
+                if (member_id.next()) {
+                    group.getMember().get(0).setId(member_id.getInt(1));
+                }
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
         return true;
     }
     
+    public boolean addMember(Group g,GroupMember member){
+        String sql = "INSERT INTO tblGroupMember(tblGroupMember.timeJoined,tblGroupMember.GroupID,tblGroupMember.playerID) "
+                + "VALUES (?, ?,?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, String.valueOf(java.time.LocalDateTime.now()));
+            ps.setInt(2, g.getId());
+            ps.setInt(3, member.getPlayer().getId());
+            ps.executeUpdate();
+
+            // id of new member
+            ResultSet member_id = ps.getGeneratedKeys();
+            if (member_id.next()) {
+                member.setId(member_id.getInt(1));
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     //search a member based on name
     public ArrayList<Group> searchGroup(String key) {
         ArrayList<Group> res = new ArrayList<>();
@@ -68,4 +105,6 @@ public class GroupDAO extends DAO{
         }
         return res;
     }
+    
+    
 }
