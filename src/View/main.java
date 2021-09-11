e64e4bfc9afffe828b49001b95508ce645e154f7
Phan200 <chualeo58@gmail.com>
2021-09-11 18:41:04 +0700

    empty main

--- src/View/main.java
+++ src/View/main.java
@@ -5,18 +5,6 @@
  */
 package View;
 
-import Controller.GameMatchDAO;
-import Controller.GroupDAO;
-import Controller.GroupMemberDAO;
-import Model.GameMatch;
-import Model.Group;
-import Model.GroupMember;
-import Model.Player;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 /**
  *
  * @author demo
@@ -25,37 +13,6 @@ public class main {
 
     public static void main(String[] args) {
 
-//        Group group = new Group();
-          GroupDAO groupDAO = new GroupDAO();
-//        group.setFounderName("luckyMan");
-//        group.setName("LaLaLa");
-//        group.setTimeStarted(String.valueOf(java.time.LocalDateTime.now()));
-//        groupDAO.createGroup(group);
 
-        List<Group> listGroup = groupDAO.searchGroup("a");
-        for (Group g : listGroup) {
-            System.out.println(String.valueOf(g.getId())+"/n");
-        }
-
-        GroupMember gm = new GroupMember();
-        gm.setGroup(listGroup.get(1));
-        gm.setPlayer(new Player(3, "12", "12", "12", "123", new ArrayList<Player> ()));
-        gm.setTimeJoined(String.valueOf(java.time.LocalDateTime.now()));
-        gm.setId(8);
-        GroupMemberDAO gmd = new GroupMemberDAO();
-        //gmd.addMember(gm);
-        //gmd.cancleMember(gm);
-        List<GroupMember> l = gmd.searchMember("c",listGroup.get(0));
-        for(GroupMember p:l){
-            System.out.println(p.getId()+" "+p.getTimeJoined());
-        }
-        
-        GameMatch match = new GameMatch();
-        match.setDescription("Let's play with us!");
-        match.setMaxPlayer(4);
-        match.setType("Random");
-        GameMatchDAO matchDAO= new GameMatchDAO();
-        
-        
     }
 }
