22bf41f909a6ff3b8f6de9c56ec17f87a401272f
Phan200 <chualeo58@gmail.com>
2021-09-16 21:40:18 +0700

    update some methods

--- src/Controller/GroupDAO.java
+++ src/Controller/GroupDAO.java
@@ -106,5 +106,70 @@ public class GroupDAO extends DAO {
         return res;
     }
     
+    public boolean cancleMember(GroupMember member) {
+        String sql = "DELETE FROM tblGroupMember WHERE tblGroupMember.id = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, member.getId());
+            ps.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+    
+    public boolean addMember(GroupMember member,int g) {
+        String sql = "INSERT INTO tblGroupMember(tblGroupMember.timeJoined,tblGroupMember.GroupID,tblGroupMember.playerID) "
+                + "VALUES (?, ?,?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, String.valueOf(java.time.LocalDateTime.now()));
+            ps.setInt(2, g);
+            ps.setInt(3, member.getPlayer().getId());
+            ps.executeUpdate();
+
+            // id of new member
+            ResultSet member_id = ps.getGeneratedKeys();
+            if (member_id.next()) {
+                member.setId(member_id.getInt(1));
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    //search all member in a group
+    public ArrayList<GroupMember> searchMember(String key,int groupID) {
+        ArrayList<GroupMember> res = new ArrayList<>();
+        String sql = "SELECT tblGroupMember.*,tblPlayer.name as name,tblPlayer.id as playerid FROM tblGroupMember,tblPlayer WHERE tblPlayer.name LIKE ? "
+                + "AND tblGroupMember.GroupID = ? "
+                + "AND tblPlayer.id=tblGroupMember.playerID";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, "%" + key + "%");
+            ps.setInt(2, groupID);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                GroupMember member = new GroupMember();
+                member.setId(rs.getInt("tblGroupMember.id"));
+                member.setTimeJoined(rs.getString("timeJoined"));
+                Player p = new Player();
+                p.setName(rs.getString("name"));
+                p.setId(rs.getInt("playerid"));
+                
+                member.setPlayer(p);
+                res.add(member);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+    
     
 }
